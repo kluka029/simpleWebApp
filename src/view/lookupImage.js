c119803493562b0b16d66b71536c405311a7cf6c
kluka029 <joshklukas123@gmail.com>
2021-12-12 21:32:11 -0600

    get rid of extra junk

--- src/view/lookupImage.js
+++ src/view/lookupImage.js
@@ -2,27 +2,6 @@ ns.view.lookupImage =
 {
 	init: function () 
 	{
-		/*
-		var table = document.getElementById("tableImage");
-		while(table.hasChildNodes())
-		{
-		   table.removeChild(table.firstChild);
-		}
-		var row={};
-		Image.loadFromLocalStorage();
-		imageSetLength = Image.imageSet.length;
-		for (var i=0; i < imageSetLength; i++) 
-		{
-			var row = table.insertRow(0);
-			var cell = row.insertCell(0);
-			cell.innerHTML = Image.imageSet[i].imageName;
-		}
-		var tableEntry = "No Images";
-		if(Image.imageSet.length > 0)
-		{
-			document.getElementById('tableImage').innerHTML = Image.imageSet[idx].imageName;
-		}
-		*/
 		var cellValue = "no images";
 		Image.loadFromLocalStorage();
 		if(Image.imageSet.length > 0)
@@ -31,13 +10,7 @@ ns.view.lookupImage =
 		}
 		document.getElementById('currentImageName').innerHTML = cellValue;
 	},
-	
-	
-	handleFirstImageAdded: function()
-	{
-		document.getElementById('currentImageName').innerHTML = "no images";
-		
-  
+
 	handleLeftButtonClickEvent: function()
 	{
 		var idx = 0
@@ -75,6 +48,5 @@ ns.view.lookupImage =
 			Image.currentImageIdx = idx;
 		}
 	}
-	  
-
+	 
 };
